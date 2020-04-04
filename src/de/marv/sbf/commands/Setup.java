083893416e9bf8478d25d703571c3e49e53449ae
Marvin Brückner <marvin.brueckner@gmx.net>
2020-04-04 10:49:31 +0200

    Some Changes

--- src/de/marv/sbf/commands/Setup.java
+++ src/de/marv/sbf/commands/Setup.java
@@ -1,14 +1,26 @@
 package de.marv.sbf.commands;
 
+import de.marv.sbf.utils.Data;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Setup implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
 
+        Player p = (Player) sender;
 
+        if(cmd.getName().equalsIgnoreCase("setup")) {
+            if(p.hasPermission("bsf.admin") || p.hasPermission("sbf.*")) {
+                if(args.length == 0) {
+                    
+                } else {
+                    p.sendMessage(Data.use + "/setup");
+                }
+            }
+        }
         return false;
     }
 }
